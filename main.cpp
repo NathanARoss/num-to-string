d5a33bf047eb68a9fa8b9c4336e3b8402a08b395
Nathan Ross <NathanAnthonyROss19@gmail.com>
2018-12-21 18:04:52 -0800

    additional work on dtoa

--- main.cpp
+++ main.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
+#include <iomanip>
 #include <math.h> //used just for INFINITY and NAN
 
 int ltoa(long num, char* chars);
 int dtoa(double num, char* chars);
 void testltoa(long l);
-void testdtoa(double d);
+void testdtoa();
+double getWholeDigits(int exponent);
 
 int main() {
     //testltoa(123456789123456789);
     //testltoa(0x8000000000000000);
 
-    testdtoa(INFINITY);
-    testdtoa(-INFINITY);
-    testdtoa(NAN);
-    testdtoa(0.0);
-    testdtoa(-0.0);
-    testdtoa(1.0);
-    testdtoa(3.14159);
+    testdtoa();
 }
 
 int lengthOfLong(unsigned long num) {
@@ -73,7 +69,28 @@ int ltoa(long num, char* chars) {
     return length;
 }
 
+double getWholeDigits(int exponent) {
+    double result = 1.0;
+    double base = 10.0;
 
+    if (exponent < 0) {
+        return 1.0;
+    }
+
+    for (;;)
+    {
+        if (exponent & 1) {
+            result *= base;
+        }
+        exponent >>= 1;
+        if (exponent == 0) {
+            break;
+        }
+        base *= base;
+    }
+
+    return result;
+}
 
 int dtoa(double num, char* chars) {
     if (num != num) {
@@ -108,28 +125,34 @@ int dtoa(double num, char* chars) {
     }
 
     int length = 0;
-    if (bits & 0x8000000000000000 != 0) {
+    if (bits >> 63 == 1) {
         chars[0] = '-';
         ++length;
+        num = -num;
     }
 
-    unsigned exponent = ((bits & 0x7FF0000000000000) >> 52) - 1023;
-    unsigned fraction = bits & 0x000FFFFFFFFFFFFF;
+    int exponent = ((bits & 0x7FF0000000000000) >> 52) - 1023;
+    unsigned long fraction = (bits & 0x000FFFFFFFFFFFFF) | 0x0070000000000000;
 
-    //TODO
-    chars[length++] = '1';
-    chars[length++] = '.';
-    for (int i = 51; i >= 0; --i) {
-        chars[length + i] = '0' + ((fraction >> i) & 1);
+    int order = ((double)exponent * log10(2.0));
+    double magnitude = getWholeDigits(order);
+    std::cout << num << " is 2^" << exponent << " mangitude " << magnitude;
+
+    if (magnitude * 10.0 <= num) {
+        magnitude *= 10.0;
+        ++order;
+        std::cout << "++";
     }
-    length += 52;
+    
+    std::cout << std::endl;
 
-    chars[length++] = ' ';
-    chars[length++] = '*';
-    chars[length++] = ' ';
-    chars[length++] = '2';
-    chars[length++] = '^';
-    length += ltoa(exponent, chars + length);     
+    // for (; order > 0; --order) {
+    //     double digit = floor(num / magnitude);
+    //     chars[length++] = (char)digit + '0';
+    //     num -= digit * magnitude;
+    //     magnitude *= 0.1;
+    // }
+
 
     return length;
 }
@@ -151,14 +174,26 @@ void testltoa(long l) {
     delete[] out;
 }
 
-void testdtoa(double d) {
+void testdtoa() {
     char* out = new char[256];
+    double tests[] = {0.0, 1.0, 2.0, 10.0, 11.0, 125.0, 11123456.0, 100000000000.0};
 
-    int length =  dtoa(d, out);
-    out[length] = 0;
-    std::cout << "std: " <<  d << std::endl;
-    std::cout << "me:  " << out << std::endl;
-    std::cout << std::endl;
+    for (double d = 1e-308; d <= 1e308; d *= 10.0) {
+        int length =  dtoa(d, out);
+        out[length] = 0;
+        //std::cout << "std: " << std::fixed << std::left << std::setw(20) << d << "me:  " << out << std::endl;
+    }
 
     delete[] out;
+}
+
+void printDoubleComponents(double f) {
+    unsigned long bits = *reinterpret_cast<unsigned long*>(&f);
+
+    bool sign = bits >> 63;
+    unsigned long exponent = (bits & 0x7FF0000000000000) >> 52;
+    unsigned long fraction = bits & 0x000FFFFFFFFFFFFF;
+
+
+    std::cout << (sign ? "-" : "") << fraction << " * 2^" << exponent << std::endl;
 }
